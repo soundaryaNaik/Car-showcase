3e84526b290708e7f4e1ca189337254743ec4723
soundaryaNaik <sonu.organizein@gmail.com>
2023-05-21 20:48:10 +0530

    color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,22 +12,19 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
       },
       colors: {
-        "black-100": "#2B2C35",
-        "primary-blue": {
-          DEFAULT: "#2B59FF",
-          100: "#F5F8FF",
-        },
+        "white-600": "#fcfcfc",
+        "black-100": "#bbbbbb",
+        "black-300": "#9195a1",
+        "black-400": "#2C2E33",
+        "primary-purple": "#3b3c98",
         "secondary-orange": "#f79761",
-        "light-white": {
-          DEFAULT: "rgba(59,60,152,0.03)",
-          100: "rgba(59,60,152,0.02)",
-        },
-        grey: "#747A88",
+
+        "primary-purple-100": "rgba(59, 60, 152, 0.1)",
       },
       backgroundImage: {
-        'pattern': "url('/pattern.png')",
-        'hero-bg': "url('/hero-bg.png')"
-      }
+        pattern: "url('/pattern.png')",
+        "hero-bg": "url('/hero-bg.png')",
+      },
     },
   },
   plugins: [],
