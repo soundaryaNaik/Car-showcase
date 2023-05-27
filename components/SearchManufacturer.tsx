9eb8e177a3a2baf53ca3b745ce2e2c1b6a6fbdec
soundaryaNaik <sonu.organizein@gmail.com>
2023-05-27 08:29:39 -0700

    search manufacturer styles

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,5 +1,5 @@
-import { Fragment, useState } from "react";
 import Image from "next/image";
+import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 
 import { manufacturers } from "@constants";
@@ -22,7 +22,7 @@ const SearchManufacturer = ({
         );
 
   return (
-    <div className="flex-1 max-sm:w-full flex justify-start items-center">
+    <div className="search-manufacturer">
       <Combobox value={manufacturer} onChange={setManuFacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
@@ -35,7 +35,7 @@ const SearchManufacturer = ({
             />
           </Combobox.Button>
           <Combobox.Input
-            className="w-full h-[52px] pl-12 p-4 rounded-l-full max-sm:rounded-full bg-light-white outline-none text-white-800 cursor-pointer"
+            className="search-manufacturere__input"
             displayValue={(item: string) => item}
             onChange={(event) => setQuery(event.target.value)}
             placeholder="bmw..."
@@ -48,14 +48,11 @@ const SearchManufacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-              static
-            >
+            <Combobox.Options className="search-manufacturere__options" static>
               {filtereManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
                   value={query}
-                  className="cursor-default select-none py-2 pl-10 pr-4"
+                  className="search-manufacturere__option"
                 >
                   Create "{query}"
                 </Combobox.Option>
@@ -64,7 +61,7 @@ const SearchManufacturer = ({
                   <Combobox.Option
                     key={item}
                     className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      `relative search-manufacturere__option ${
                         active ? "bg-primary-blue text-white" : "text-gray-900"
                       }`
                     }
