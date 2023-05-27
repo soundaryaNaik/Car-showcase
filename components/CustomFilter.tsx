ad7a464c364f11e79b637aeaaf4b6a87eda573ef
soundaryaNaik <sonu.organizein@gmail.com>
2023-05-27 08:13:22 -0700

    custom filter styles

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -27,7 +27,7 @@ export default function CustomFilter({ title, options }: CustomFilterProps) {
         }}
       >
         <div className="relative w-fit z-10">
-          <Listbox.Button className="relative w-full min-w-[127px] flex justify-between items-center cursor-default rounded-lg bg-white py-2 px-3 text-left shadow-md sm:text-sm border">
+          <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
             <Image
               src="/chevron-up-down.svg"
@@ -43,7 +43,7 @@ export default function CustomFilter({ title, options }: CustomFilterProps) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="custom-filter__options">
               {options.map((option) => (
                 <Listbox.Option
                   key={option.title}
